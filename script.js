bb5cda28b32019d14487a39bd79d561bb964c3c3
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-03 22:29:26 -0500

    Update script.js
    Create function "calculateTime"

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ const start = () => {
         stopwatch.textContent = calculateTime(runningTime);
     }, 1000)
 }
+
+const calculateTime = runningTime => {
+    const total_seconds = Math.floor(runningTime / 1000);
+    const total_minutes = Math.floor(total_seconds / 60);
+
+    const display_seconds = (total_seconds % 60).toString().padStart(2, "0");
+    const display_minutes = total_minutes.toString().padStart(2, "0");
+
+    return `${display_minutes}:${display_seconds}`
+}
