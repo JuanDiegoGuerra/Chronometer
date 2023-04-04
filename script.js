034ba190cca78a1b6b3df4fd6c4cf244ce595909
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-03 22:27:17 -0500

    Update script.js
    Create "playPause" function.

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const secondsSphere = document.getElementById('seconds-sphere');
 
 let stopwatchInterval;
 let runningTime = 0;
+
+const playPause = () => {
+    const isPaused = !playPauseButton.classList.contains('running');
+    if (isPaused) {
+        playPauseButton.classList.add('running');
+        start();
+    } else {
+        playPauseButton.classList.remove('running');
+        pause();
+    }
+}
