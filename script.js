7a55964a8dcc5ea1ae4bb9e914ca953764156e61
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-03 22:26:26 -0500

    Update script.js
    Added "const and let" variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const stopwatch = document.getElementById('stopwatch');
+const playPauseButton = document.getElementById('play-pause');
+const secondsSphere = document.getElementById('seconds-sphere');
+
+let stopwatchInterval;
+let runningTime = 0;
